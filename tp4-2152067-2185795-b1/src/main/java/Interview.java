cdc2fa94d1ae658792ab71aa3dfe637d57e7f842
Rodrigo Merino Martel <rodrimerino201@gmail.com>
2023-03-13 22:35:32 -0400

    Add files via upload

---
+++ tp4-2152067-2185795-b1/src/main/java/Interview.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public final class Interview {
+    /**
+     * Expliquez votre complexité temporelle et spatiale à l'aide de commentaire dans le code
+     * Indiquez les équivalences telles que O(n + 1) => O(n) et O(2n) => O(n)
+     *
+     * * TODO Time Complexity  :
+     * * TODO Space Complexity :
+     *
+     * @param boxes The array that contains the weight of each box.
+     * @return The weight of the final box if it is applicable.
+     */
+    static public int lastBox(int[] boxes) {
+        return -1;
+    }
+}
